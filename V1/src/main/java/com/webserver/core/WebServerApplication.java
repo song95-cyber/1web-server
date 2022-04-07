a28675b3f4ef3121e6760c71a7ab5f8a0811c5fa
TEDU <83123095@qq.com>
2022-04-07 14:26:30 +0800

    新项目创建

---
+++ V1/src/main/java/com/webserver/core/WebServerApplication.java
@@ -0,0 +1,15 @@
+package com.webserver.core;
+
+/**
+ * WebServer主类
+ * WebServer是一个Web容器，实现了Tomcat中的基础功能，通过这个项目的学习了解Tomcat的
+ * 底层工作逻辑。
+ * Web容器是一个Web服务端程序，主要负责两方面的工作:
+ * 1:管理部署在容器中的所有网络应用
+ * 2:与客户端(通常就是浏览器)进行TCP通讯，并基于HTTP协议进行应用交互，使得客户端可以
+ *   通过网络远程调用容器下不同网络应用中的内容。
+ *
+ * 网络应用(webapp):包含的内容大致有:网页，处理业务的代码，其他资源等。就是俗称的"网站"
+ */
+public class WebServerApplication {
+}
